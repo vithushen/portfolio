c60cfbe441b7acf12192027b5f928f052f8ce536
vithushen <vithu.99@hotmail.com>
2023-10-16 19:53:43 -0400

    [Portfolio-5]-Fixed CSS for my Projects and Skills

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import vithuImage from './assets/vithu.png';
 import AboutMePopup from './AboutMePopup';
 import eldr from './assets/eldr.png';
-import fantasy from './assets/fantasy.png';
-import datum from './assets/datum.png';
+import fantasy from './assets/fantasy_nba.png';
+import datum from './assets/datum_monitor.png';
+import covidex from './assets/covidex.png';
 
 
 // Import skill logos
@@ -98,7 +99,9 @@ function App() {
             <p className='description' style={{ color: "white", textAlign: 'left' }}>The purpose of this project is to design a mobile application to allow researchers to collect data for a specific study that encourages repeated engagement through gamification.</p>
             <p className='description' style={{ color: "white", textAlign: 'left' }}>A user-centered approach will be applied to encourage usability and engagement. The application would be usable for a variety of different research studies (e.g. mental health studies, product engagement studies, etc.). Hence, it could be used in a wide array of fields.</p>
             <div>
-              <button className='preview'>Preview</button>
+              <button className='preview' onClick={() => { window.location.href = 'https://datum-io.app/user/dashboard'; }}>
+                Preview
+              </button>
               <button className='source'>Source</button>
             </div>
           </div>
@@ -109,12 +112,14 @@ function App() {
             <p className='description' style={{ color: "white", textAlign: 'left' }}>Covidex is a covid-19 tracking application designed for multiple users such as common patients, doctors, health officials and and immigration officials. Its goal is to help us deal with the reality of the situation we currently face and try to move forward.</p>
             <p className='description' style={{ color: "white", textAlign: 'left' }}>Patients would be able to upload their health status details onto the platform. This in turn would be monitored by both doctors and health officials to both help the patients as well as track the progress of the virus. We would be able to monitor daily cases and get a better understanding of how the virus is changing to help mitigate the risks for the rest of the population.</p>
             <div>
-              <button className='preview'>Preview</button>
+              <button className='preview' onClick={() => { window.location.href = 'https://covidex.live/'; }}>
+                Preview
+              </button>
               <button className='source'>Source</button>
             </div>
           </div>
           <div className='second-container'>
-            <img src={eldr} alt='Project' className='eldr-image' />
+            <img src={covidex} alt='Project' className='eldr-image' />
           </div>
         </div>
         <div className='project-container'>
@@ -126,7 +131,9 @@ function App() {
             <p className='description' style={{ color: "white", textAlign: 'left' }}>NBA Fantasy Draft Helper is your ultimate companion for building a winning fantasy basketball team! This innovative project brings together the power of data and user-friendly design, providing NBA enthusiasts with a comprehensive tool to make the best decisions during their fantasy drafts.</p>
             <p className='description' style={{ color: "white", textAlign: 'left' }}>NBA Stats Explorer simplifies the process of understanding player performance by offering a visually appealing and easy-to-navigate platform. Whether you're a fantasy basketball enthusiast, a sports analyst, or just a passionate NBA fan, this tool provides a unique perspective on player capabilities, fostering a deeper appreciation for the game.</p>
             <div>
-              <button className='preview'>Preview</button>
+              <button className='preview' onClick={() => { window.location.href = 'https://vithushen.github.io/NBA-Fantasy-Helper/'; }}>
+                Preview
+              </button>
               <button className='source'>Source</button>
             </div>
           </div>
@@ -137,7 +144,9 @@ function App() {
             <p className='description' style={{ color: "white", textAlign: 'left' }}>Eldr is a friendly app made for elderly people. It helps them talk to others, play games, and make new friends easily. The app is easy to use and makes seniors feel less lonely by connecting them with others.</p>
             <p className='description' style={{ color: "white", textAlign: 'left' }}>The Figma project and mockup website show how Eldr works, making it simple for everyone to understand its purpose: bringing happiness and friendship to older adults. Eldr is all about making seniors' lives better by connecting them with others in a fun and easy way.</p>
             <div>
-              <button className='preview'>Preview</button>
+              <button className='preview' onClick={() => { window.location.href = 'https://www.figma.com/proto/AeMKmKMeebpAjkK44w4yGK/Soen-357-Mini-Project?node-id=39-689&starting-point-node-id=39%3A590'; }}>
+                Preview
+              </button>
               <button className='source'>Source</button>
             </div>
           </div>
