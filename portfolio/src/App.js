0969070efc1e2ed59b2df5fe39472b4e2fbb37d7
vithushen <vithu.99@hotmail.com>
2024-04-24 19:39:34 -0400

    [Portfolio-20]-Added changes to my projects blip

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,13 +3,7 @@ import Typewriter from 'typewriter-effect'
 import './App.css';
 import vithuImage from './assets/vithu.png';
 import AboutMePopup from './AboutMePopup';
-import eldr from './assets/eldr.png';
-import fantasy from './assets/fantasy_nba.png';
-import datum from './assets/datum_monitor.png';
-import covidex from './assets/covidex.png';
-import mave from './assets/mave.png';
 import github from './assets/github.png';
-import linkedin from './assets/linkedin.png';
 
 import datum1 from './assets/datum-1.png';
 import datum2 from './assets/datum-2.png';
@@ -27,11 +21,11 @@ import covidex2 from './assets/covidex-2.png';
 import covidex3 from './assets/covidex-3.png';
 
 import nba1 from './assets/nba-1.png';
-import nba2 from './assets/nba-2.jpeg'; 
+import nba2 from './assets/nba-2.jpeg';
 
 import eldr1 from './assets/elder-1.png';
 import eldr2 from './assets/elder-2.png';
-import eldr3 from './assets/elder-3.png'; 
+import eldr3 from './assets/elder-3.png';
 
 import ryu1 from './assets/ryu-1.png';
 import ryu2 from './assets/ryu-2.png';
@@ -39,6 +33,13 @@ import ryu3 from './assets/ryu-3.png';
 import ryu4 from './assets/ryu-4.png';
 import ryu5 from './assets/ryu-5.png';
 
+import blip1 from './assets/blip-1.png';
+import blip2 from './assets/blip-2.png';
+import blip3 from './assets/blip-3.png';
+import blip4 from './assets/blip-4.png';
+import blip5 from './assets/blip-5.png';
+import blip6 from './assets/blip-6.png';
+import blip7 from './assets/blip-7.png';
 
 // Import skill logos
 import htmlLogo from './assets/html.png';
@@ -64,6 +65,7 @@ import VisualStudioLogo from './assets/visual-studio-code.png';
 import DockerLogo from './assets/docker.png';
 import vithuCV from './assets/vithuCV.pdf';
 
+import blipVideo from './assets/blip.mkv'; // Import the video file
 
 
 
@@ -78,8 +80,17 @@ function App() {
     setIsAboutMePopupOpen(false);
   };
 
+  //Video
+  const [isVideoPopupOpen, setIsVideoPopupOpen] = useState(false); // State for video popup
+  // Function to open the video popup
+  const openVideoPopup = () => {
+    setIsVideoPopupOpen(true);
+  };
 
-
+  // Function to close the video popup
+  const closeVideoPopup = () => {
+    setIsVideoPopupOpen(false);
+  };
 
   // Programming languages
   const programmingLanguages = [
@@ -135,6 +146,7 @@ function App() {
     project4: 0,
     project5: 0,
     project6: 0,
+    project7: 0,
   });
 
   // Define images for Datum.io 
@@ -143,20 +155,24 @@ function App() {
   // Define images for Covidex
   const project2Images = [covidex2, covidex1, covidex3];
 
-   // Define images for NBA Fantasy Helper
-   const project3Images = [nba1, nba2];
+  // Define images for NBA Fantasy Helper
+  const project3Images = [nba1, nba2];
 
-    // Define images for Mave Grocery
-    const project4Images = [mave1, mave2, mave3, mave4];
+  // Define images for Mave Grocery
+  const project4Images = [mave1, mave2, mave3, mave4];
 
-   // Define images for Eldr
-   const project5Images = [eldr1, eldr3 ,eldr2];
+  // Define images for Eldr
+  const project5Images = [eldr1, eldr3, eldr2];
 
-    // Define images for RYU Inventory
-   const project6Images = [ryu1, ryu2 ,ryu3, ryu4, ryu5];
+  // Define images for RYU Inventory
+  const project6Images = [ryu1, ryu2, ryu3, ryu4, ryu5];
 
 
-   const handleNextImage = (project) => {
+  // Define images for Blip
+  const project7Images = [blip1, blip2, blip3, blip4, blip5, blip6, blip7];
+
+
+  const handleNextImage = (project) => {
     setCurrentProjectImageIndexes((prevState) => {
       const projectImagesLength = {
         project1: project1Images.length,
@@ -165,15 +181,16 @@ function App() {
         project4: project4Images.length,
         project5: project5Images.length,
         project6: project6Images.length,
+        project7: project7Images.length,
       };
-  
+
       return {
         ...prevState,
         [project]: (prevState[project] + 1) % projectImagesLength[project],
       };
     });
   };
-  
+
   const handlePrevImage = (project) => {
     setCurrentProjectImageIndexes((prevState) => {
       const projectImagesLength = {
@@ -182,9 +199,9 @@ function App() {
         project3: project3Images.length,
         project4: project4Images.length,
         project5: project5Images.length,
-        project6: project6Images.length,
+        project7: project7Images.length,
       };
-  
+
       return {
         ...prevState,
         [project]: (prevState[project] - 1 + projectImagesLength[project]) % projectImagesLength[project],
@@ -271,45 +288,62 @@ function App() {
 
       <div className="projects" id="projects">
         <h1 className="title-project" style={{ fontSize: "50px" }}>
-          My Projects/Experiences
+          My Projects
         </h1>
 
-           {/* Project 1 */}
-           <div className="project-container">
+        {/* Project 1 */}
+        <div className="project-container">
           <div className="project-details">
             <h2>Blip Barber</h2>
             <p className="description">
-              The purpose of this project is to design a mobile application to allow researchers to collect data for a specific study that encourages repeated engagement through gamification.
+              Our platform serves as a dynamic marketplace, connecting skilled home barbers with customers seeking affordable and convenient haircuts right in their neighborhood.
               <br></br>
               <br></br>
-              A user-centered approach will be applied to encourage usability and engagement. The application would be usable for a variety of different research studies (e.g. mental health studies, product engagement studies, etc.). Hence, it could be used in a wide array of fields.
+              Designed with both barbers and customers in mind, our application offers a seamless experience for both parties. Barbers gain a powerful platform to showcase their services and reach a broader audience, while customers can easily browse and book appointments with trusted barbers nearby. Stay tuned as we continue to develop this exciting project, shaping the future of home barbering and enhancing accessibility to quality haircare services for all.
             </p>
             <div>
-              <button className="button-preview-source" onClick={() => window.open('https://datum-io.app/user/dashboard', '_blank')}>Demo</button>
+              <button className="button-preview-source" onClick={openVideoPopup}>Video Demo</button>
               <button className="button-preview-source" onClick={() => window.open('https://github.com/vithushen/blip', '_blank')}>Source</button>
             </div>
           </div>
           <div className="project-image">
-            <img className='image' src={project1Images[currentProjectImageIndexes.project1]} alt="Project 1" />
+            <img className='image' src={project7Images[currentProjectImageIndexes.project7]} alt="Project 7" />
             <div>
-              <button className='button-prev-next' onClick={() => handlePrevImage("project1")}>Previous</button>
-              <button className='button-prev-next' onClick={() => handleNextImage("project1")}>Next</button>
+              <button className='button-prev-next' onClick={() => handlePrevImage("project7")}>Previous</button>
+              <button className='button-prev-next' onClick={() => handleNextImage("project7")}>Next</button>
             </div>
           </div>
         </div>
 
-           {/* Project 1 */}
-           <div className="project-container">
+
+        {/* Video Popup */}
+        {isVideoPopupOpen && (
+          <div className="video-popup">
+            <video
+              controls
+              autoPlay
+              onEnded={closeVideoPopup}
+            >
+              <source src={blipVideo} />
+              Your browser does not support the video tag.
+            </video>
+            {/* Close button */}
+            <button className="close-video-popup" onClick={closeVideoPopup}>X</button>
+          </div>
+        )}
+
+        {/* Project 1 */}
+        <div className="project-container">
           <div className="project-details">
             <h2>RYU Inventory</h2>
             <p className="description">
-              The purpose of this project is to design a mobile application to allow researchers to collect data for a specific study that encourages repeated engagement through gamification.
+              A user-friendly web application designed for the Ryu Restaurant manager to efficiently keep track and maintaining their inventory. With our intuitive interface, managers can effortlessly perform inventory counts at the end of each month, ensuring accurate reporting.
               <br></br>
               <br></br>
-              A user-centered approach will be applied to encourage usability and engagement. The application would be usable for a variety of different research studies (e.g. mental health studies, product engagement studies, etc.). Hence, it could be used in a wide array of fields.
+              Our application allows users to easily add, remove, or edit products within their inventory database, providing ultimate flexibility and control. Before using our application, they were using excel spreadsheets but now making it much more simple with a digital approach to inventory management.
             </p>
             <div>
-              <button className="button-preview-source" onClick={() => window.open('', '_blank')}>Demo</button>
+              <button className="button-preview-source" onClick={() => window.open('', '_blank')}>Website Demo</button>
               <button className="button-preview-source" onClick={() => window.open('https://github.com/vithushen/ryu-lavia', '_blank')}>Source</button>
             </div>
           </div>
@@ -333,7 +367,7 @@ function App() {
               A user-centered approach will be applied to encourage usability and engagement. The application would be usable for a variety of different research studies (e.g. mental health studies, product engagement studies, etc.). Hence, it could be used in a wide array of fields.
             </p>
             <div>
-              <button className="button-preview-source" onClick={() => window.open('https://datum-io.app/user/dashboard', '_blank')}>Demo</button>
+              <button className="button-preview-source" onClick={() => window.open('https://datum-io.app/user/dashboard', '_blank')}>Website Demo</button>
               <button className="button-preview-source" onClick={() => window.open('https://github.com/HamzahSheikh/datum.io', '_blank')}>Source</button>
               <button className="button-preview-source" onClick={() => window.open('https://drive.google.com/file/d/1dWGf5BkdpLyCr9IBWnFkdbajuKIjNxYc/view', '_blank')}>Document</button>
             </div>
@@ -358,7 +392,7 @@ function App() {
               Patients would be able to upload their health status details onto the platform. This in turn would be monitored by both doctors and health officials to both help the patients as well as track the progress of the virus. We would be able to monitor daily cases and get a better understanding of how the virus is changing to help mitigate the risks for the rest of the population.
             </p>
             <div>
-              <button className="button-preview-source" onClick={() => window.open('https://covidex.live/', '_blank')}>Demo</button>
+              <button className="button-preview-source" onClick={() => window.open('https://covidex.live/', '_blank')}>Website Demo</button>
               <button className="button-preview-source" onClick={() => window.open('https://github.com/HamzahSheikh/covidex-public', '_blank')}>Source</button>
               <button className="button-preview-source" onClick={() => window.open('https://drive.google.com/file/d/18mJfjZiyo1a1wZsZAaJdHwSCR0GijIVf/view', '_blank')}>Document</button>
             </div>
@@ -383,7 +417,7 @@ function App() {
               NBA Stats Explorer simplifies the process of understanding player performance by offering a visually appealing and easy-to-navigate platform. Whether you're a fantasy basketball enthusiast, a sports analyst, or just a passionate NBA fan, this tool provides a unique perspective on player capabilities, fostering a deeper appreciation for the game.
             </p>
             <div>
-              <button className="button-preview-source" onClick={() => window.open('https://vithushen.github.io/NBA-Fantasy-Helper/', '_blank')}>Demo</button>
+              <button className="button-preview-source" onClick={() => window.open('https://vithushen.github.io/NBA-Fantasy-Helper/', '_blank')}>Website Demo</button>
               <button className="button-preview-source" onClick={() => window.open('https://github.com/vithushen/NBA-Fantasy-Helper', '_blank')}>Source</button>
             </div>
           </div>
@@ -407,7 +441,7 @@ function App() {
               Our user-friendly interface allows customers to browse through a catalog, add items to their cart and securely complete their purchases online. With simple navigation, secure payment options and timely delivery services, our web application redefines the way people shop for groceries, making it effortless, efficient, and enjoyable.
             </p>
             <div>
-              <button className="button-preview-source" onClick={() => window.open('https://mave-grocery.web.app/index.html', '_blank')}>Demo</button>
+              <button className="button-preview-source" onClick={() => window.open('https://mave-grocery.web.app/index.html', '_blank')}>Website Demo</button>
               <button className="button-preview-source" onClick={() => window.open('https://github.com/Ellendra-JH/mave')}>Source</button>
             </div>
           </div>
@@ -431,7 +465,7 @@ function App() {
               The Figma project and mockup website show how Eldr works, making it simple for everyone to understand its purpose: bringing happiness and friendship to older adults. Eldr is all about making seniors' lives better by connecting them with others in a fun and easy way.
             </p>
             <div>
-              <button className="button-preview-source" onClick={() => window.open('https://www.figma.com/proto/AeMKmKMeebpAjkK44w4yGK/Soen-357-Mini-Project?node-id=39-689&starting-point-node-id=39%3A590', '_blank')}>Demo</button>
+              <button className="button-preview-source" onClick={() => window.open('https://www.figma.com/proto/AeMKmKMeebpAjkK44w4yGK/Soen-357-Mini-Project?node-id=39-689&starting-point-node-id=39%3A590', '_blank')}>Website Demo</button>
             </div>
           </div>
           <div className="project-image">
